f562568550183f479b7a3d1c906654236568e509
AndresPoz <andres-pozzer@hotmail.com>
2023-04-30 11:47:51 -0300

    modificamos la salida

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main{
     public static void main(String[] args){
         System.out.println("Hola mundo, desde java");
-        System.out.println("Realizada la conexion con github");
+        System.out.println("Ya todo funciona, ahora ejecutmos cambios en la rama");
     }
 }
